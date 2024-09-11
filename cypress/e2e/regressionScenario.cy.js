e3db606770fd898ab90c20ff38d8b274335eb281
Yana <51543583+yanastasevich@users.noreply.github.com>
2024-09-11 09:58:59 +0400

    Update regressionScenario.cy.js

---
+++ cypress/e2e/regressionScenario.cy.js
@@ -0,0 +1,39 @@
+// a stub 
+describe('Perform regression testing', () => {
+
+
+
+    before(() => {
+      // Run before all tests
+      cy.performLogin();
+      cy.visitMap();
+    });
+
+    it('Add a polygon with 5 vertices and valid coordinates though polygon management system', () => { 
+       
+    });
+
+    it('Update all coordinates with valid values through polygon management system', () => { 
+    });
+
+    it('Update polygon name', () => { 
+     });
+
+     it('Switch to marker management system', () => { 
+     });
+
+     it('Add a marker through marker management system', () => { 
+     });
+
+     it('Update the name of the marker', () => { 
+     });
+
+     it('Update the coordinates of the marker', () => { 
+     });
+
+     it('Remove the polygon through the polygon management system', () => { 
+     });
+
+     it('Remove the marker through marker management system', () => { 
+     });
+});
